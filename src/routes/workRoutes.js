6cf0843097a3a71f407c8d804627fb75b72bb5c4
Abreu <renata.braga.d.abreu@accenture.com>
2020-11-05 14:23:51 -0300

    add missing cors

--- src/routes/workRoutes.js
+++ src/routes/workRoutes.js
@@ -5,9 +5,9 @@ const cors = require('cors')
 
 router.get('/coppola_works', cors(), controller.getAll);
 router.get('/coppola_works/:_id', cors(), controller.getById);
-router.post('/coppola_works', controller.addWork)
-router.put('/coppola_works/:_id', controller.updateWork)
-router.delete('/coppola_works/:_id', controller.deleteWork)
+router.post('/coppola_works', cors(), controller.addWork)
+router.put('/coppola_works/:_id', cors(), controller.updateWork)
+router.delete('/coppola_works/:_id', cors(), controller.deleteWork)
 
 router.get('/coppola_works_as_director', cors(), controller.getAllAsDirector);
 
